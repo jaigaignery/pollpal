195ff9129b34960caddc31b94dd4bb885f5b0ab4
Jai Gaignery <jaigaignery@gmail.com>
2023-11-26 22:28:51 -0500

    Revert hosting changes"

--- firebase.js
+++ firebase.js
@@ -10,7 +10,7 @@ const {
   Filter,
 } = require("firebase-admin/firestore");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+const serviceAccount = require("./pollpal-server-cred.json");
 
 initializeApp({
   credential: cert(serviceAccount),
